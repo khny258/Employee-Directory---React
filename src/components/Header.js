ea97fb70a2beaea92056cb6c554976b597f5375e
Kenny Huang <khny258@gmail.com>
2020-06-19 20:32:31 -0400

    small edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import "../styles/Header.css";
 function Header () {
     return (
       <div className="header">
-        <h1>Employee Directory</h1>
-        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        <h1>Employee Directory - React</h1>
+        <p>Click on 🔻 by heading to sort or use the search box to narrow your results.</p>
       </div>
     )
   }
